b2cab572e7556e8e71e8444b2399a27ce9b2478f
mergitto <merugitosu@gmail.ccom>
2018-11-26 16:37:40 +0900

    前処理をまとめたのでその関数を呼び出すようにした

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import pandas as pd
-from logistic_regression import Tree, drop_column, add_dummy_score
+from logistic_regression import Tree, preprocess
 
 df = pd.read_csv('./questionnaire_evaluations_from_1031_to_1109_all.csv')
-df = drop_column(df)
-df = add_dummy_score(df)
+df = preprocess(df)
 X = df.drop([
         "score_dummy", "report_created_date", "bm25_sum",
         "type_id", "shokushu_id", "score_std"
@@ -17,3 +16,4 @@ tree.grid_search()
 tree.learning_curve_show(save_file_name="./decision_tree_image/qustion_learning_curve.pdf")
 tree.decision_tree_classifier(save_file_name="decision_tree_image/decision_tree_questionnaire.pdf")
 tree.logistic_regression()
+
