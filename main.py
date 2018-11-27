adf47517d72e78f178225bf2228f2a890a6fbad3
mergitto <merugitosu@gmail.ccom>
2018-11-27 11:03:59 +0900

    何回も回したい奴を追加

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ df = pd.read_csv('./questionnaire_evaluations_from_1031_to_1109_all.csv')
 df = preprocess(df)
 X = df.drop([
         "score_dummy", "report_created_date", "bm25_sum",
-        "type_id", "shokushu_id", "score_std"
+        "type_id", "shokushu_id", "score_std",
+        "recommend_level",
         #"tfidf_top_average", "recommend_level",
     ], axis=1) # score_dummy以外の列を抽出
 print(X.keys())
 y = df["score_dummy"].astype(int) # score_dummyの列を抽出
 tree = Tree(X, y)
+for i in range(2, 10):
+    tree.decision_tree_classifier(save_file_name="decision_tree_image/decision_tree_questionnaire_{}.pdf".format(i), max_depth=i)
+    tree.learning_curve_show(save_file_name="./decision_tree_image/qustion_learning_curve_{}.pdf".format(i), max_depth=i)
 tree.cross_validation()
 tree.grid_search()
-tree.learning_curve_show(save_file_name="./decision_tree_image/qustion_learning_curve.pdf")
-tree.decision_tree_classifier(save_file_name="decision_tree_image/decision_tree_questionnaire.pdf")
 tree.logistic_regression()
 
