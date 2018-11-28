9686e6258aae5a1ab8d39c0c9f57e2647ebba25c
mergitto <merugitosu@gmail.ccom>
2018-11-28 17:33:47 +0900

    過去の評価分の結果も使うように修正

--- main.py
+++ main.py
@@ -10,43 +10,52 @@ drop_list = [
         "advice_divide_mecab", "bm25_average", "course_code", "created",
         "keywords", "modified", "score", "score_min_max", "search_word",
         "search_word_wakati", "topic", "st_no",
+        "most_highest_similarity",
         #'count_selection', 'diff_date', 'first_final_diff_days',
         #'identification_word_count', 'is_match_keywords',
-        #'most_highest_similarity', 'recommend_rank', 'report_created_datetime',
-        #'similarity_sum', 'tfidf_sum', 'word_length'
-    ] # 不必要なカラム
-#tree = Tree(df)
-#tree.add_dummy_score()
-#tree.drop_columns(drop_list)
-#tree.set_X_and_y(objective_key="score_dummy")
-#for i in range(2, 10):
-#    tree.decision_tree_classifier(save_file_name="decision_tree_image/decision_tree_questionnaire_{}.pdf".format(i), max_depth=i)
-#    tree.learning_curve_show(save_file_name="./decision_tree_image/qustion_learning_curve_{}.pdf".format(i), max_depth=i)
-#    tree.cross_validation(save_file_name="decision_tree_image/cross_decision_tree_{}.pdf".format(i), max_depth=i)
-#    tree.random_forest(min_samples_leaf=i, random_state=i, max_depth=i)
-#tree.grid_search()
-#tree.logistic_regression()
-
-df = pd.read_csv('./questionnaire_evaluations_preprocessed_all.csv')
-drop_list = [
-        "report_created_date", "bm25_sum",
-        "type_id", "shokushu_id", "score_std",
-        "recommend_level", "tfidf_top_average",
-        "info_date", "write_date", "first_date", "second_date", "final_date", "decision_date",
-        "advice_divide_mecab", "bm25_average", "course_code", "created",
-        "keywords", "modified", "score", "score_min_max", "search_word",
-        "search_word_wakati", "topic", "st_no",
-        "is_good", "evaluation_id", "recommend_formula",
-        #'count_selection', 'diff_date', 'first_final_diff_days',
-        #'identification_word_count', 'is_match_keywords',
-        #'most_highest_similarity', 'recommend_rank', 'report_created_datetime',
+        #'recommend_rank', 'report_created_datetime',
         #'similarity_sum', 'tfidf_sum', 'word_length'
     ] # 不必要なカラム
 tree = Tree(df)
 tree.add_dummy_score()
 tree.drop_columns(drop_list)
 tree.set_X_and_y(objective_key="score_dummy")
-print(tree.X.keys())
 for i in range(2, 10):
+    tree.decision_tree_classifier(save_file_name="decision_tree_image/decision_tree_questionnaire_{}.pdf".format(i), max_depth=i)
+    tree.learning_curve_show(save_file_name="./decision_tree_image/qustion_learning_curve_{}.pdf".format(i), max_depth=i)
+    tree.cross_validation(save_file_name="decision_tree_image/cross_decision_tree_{}.pdf".format(i), max_depth=i)
     tree.random_forest(min_samples_leaf=i, random_state=i, max_depth=i)
+tree.grid_search()
+tree.logistic_regression()
+
+
+#df = pd.read_csv('./questionnaire_evaluations_preprocessed_all.csv')
+#drop_list = [
+#        "report_created_date", "bm25_sum",
+#        "type_id", "shokushu_id", "score_std",
+#        "recommend_level", "tfidf_top_average",
+#        "info_date", "write_date", "first_date", "second_date", "final_date", "decision_date",
+#        "advice_divide_mecab", "bm25_average", "course_code", "created",
+#        "keywords", "modified", "score", "score_min_max", "search_word",
+#        "search_word_wakati", "topic", "st_no",
+#        "is_good", "evaluation_id", "recommend_formula",
+#        "most_highest_similarity",
+#        #'count_selection', 'diff_date', 'first_final_diff_days',
+#        #'identification_word_count', 'is_match_keywords',
+#        #'most_highest_similarity', 'recommend_rank', 'report_created_datetime',
+#        #'similarity_sum', 'tfidf_sum', 'word_length'
+#    ] # 不必要なカラム
+#tree = Tree(df)
+#tree.add_dummy_score()
+#tree.drop_columns(drop_list)
+#tree.set_X_and_y(objective_key="score_dummy")
+#print(tree.X.keys())
+#for i in range(2, 10):
+#    tree.random_forest(min_samples_leaf=i, random_state=i, max_depth=i)
+#    tree.decision_tree_classifier(save_file_name="decision_tree_image/decision_tree_questionnaire_{}.pdf".format(i), max_depth=i)
+#    tree.learning_curve_show(save_file_name="./decision_tree_image/qustion_learning_curve_{}.pdf".format(i), max_depth=i)
+#    tree.cross_validation(save_file_name="decision_tree_image/cross_decision_tree_{}.pdf".format(i), max_depth=i)
+#    tree.random_forest(min_samples_leaf=i, random_state=i, max_depth=i)
+#tree.grid_search()
+#tree.logistic_regression()
 
