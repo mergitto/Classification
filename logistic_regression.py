8d01722a67e968a3b9a2615d668bbb1f3d5fee7f
mergitto <merugitosu@gmail.ccom>
2018-11-28 13:42:28 +0900

    画像出力に関する処理修正

--- logistic_regression.py
+++ logistic_regression.py
@@ -24,16 +24,44 @@ class Tree():
         X_test_std = sc.transform(X_test)
         return X_train_std, X_test_std
 
-    def cross_validation(self):
+    def tmp_random_forest(self, random_state=0, min_samples_leaf=3):
+        from sklearn.ensemble import RandomForestClassifier
+        X_train, X_test, y_train, y_test = self.train_test_data_split(random_state=random_state, test_size=0.3)
+        X_train_std, X_test_std = self.std_X(X_train, X_test)
+        forest = RandomForestClassifier(min_samples_leaf=min_samples_leaf, random_state=random_state+1)
+        forest.fit(X_train_std, y_train)
+        return {
+                'train': metrics.accuracy_score(y_train, forest.predict(X_train_std)) ,
+                'test': metrics.accuracy_score(y_test, forest.predict(X_test_std))
+                }
+
+    def tmp_cross_validation(self, max_depth=2, random_state=0):
+        X_train,X_test,y_train,y_test = self.train_test_data_split(random_state=random_state, test_size=0.3)
+        X_train_std, X_test_std = self.std_X(X_train, X_test)
+
+        treeModel = DecisionTreeClassifier(max_depth=max_depth, random_state=random_state+1)
+        treeModel.fit(X_train_std, y_train)
+        return {
+                'train': metrics.accuracy_score(y_train, treeModel.predict(X_train_std)) ,
+                'test': metrics.accuracy_score(y_test, treeModel.predict(X_test_std))
+                }
+
+
+    def cross_validation(self, max_depth=2, save_file_name="decision_tree_image/tmp.pdf"):
         from sklearn.model_selection import cross_val_score
         print("    ======= 交差検証 ======")
-        print("    [max_depth, score_mean]")
-        for max_depth in [2, 3, 4, 5, 6, 7, 8, 9]:
-            clf = DecisionTreeClassifier(max_depth = max_depth)
-            score = cross_val_score(estimator = clf, X = self.X, y = self.y, cv = 5)
-            print("   ",[max_depth, score.mean()])
-            viz = dtreeviz(clf, self.X, self.y, target_name='score_dummy', feature_names=list(self.X.keys()), class_names=["high", "low"])
-            viz.save("decision_tree_image/cross_decision_tree_{}.pdf".format(max_depth))
+        clf = DecisionTreeClassifier(max_depth = max_depth)
+        score = cross_val_score(estimator = clf, X = self.X, y = self.y, cv = 5)
+        plt.figure()
+        viz = dtreeviz(clf, self.X, self.y, target_name='score_dummy', feature_names=list(self.X.keys()), class_names=["high", "low"])
+        viz.save("decision_tree_image/cross_decision_tree_{}.pdf".format(max_depth))
+        plt.close()
+
+        X_train,X_test,y_train,y_test = self.train_test_data_split(random_state=max_depth, test_size=0.3)
+        X_train_std, X_test_std = self.std_X(X_train, X_test)
+        clf.fit(X_train_std, y_train)
+        print("    [max_depth, score_mean, train_predict, test_predict]")
+        print("   ", [max_depth, score.mean(), metrics.accuracy_score(y_train, clf.predict(X_train_std)), metrics.accuracy_score(y_test, clf.predict(X_test_std))])
         print("    ============ end =============")
 
     def grid_search(self):
@@ -48,13 +76,13 @@ class Tree():
         print("   ",clf.best_params_)
         print("    ============ end =============")
 
-    def learning_curve_show(self, save_file_name):
+    def learning_curve_show(self, save_file_name, max_depth=2):
         X_train,X_test,y_train,y_test = self.train_test_data_split(random_state=1, test_size=0.3)
         from sklearn.model_selection import learning_curve
         from sklearn.pipeline import make_pipeline
         pipe_lr = make_pipeline(StandardScaler(),
                         DecisionTreeClassifier(
-                        class_weight=None, criterion='gini', max_depth=3,
+                        class_weight=None, criterion='gini', max_depth=max_depth,
                         max_features=None, max_leaf_nodes=None,
                         min_impurity_split=1e-07, min_samples_leaf=1,
                         min_samples_split=2, min_weight_fraction_leaf=0.0,
@@ -70,6 +98,7 @@ class Tree():
         train_std = np.std(train_scores, axis=1)
         test_mean = np.mean(test_scores, axis=1)
         test_std = np.std(test_scores, axis=1)
+        plt.figure()
         plt.plot(train_sizes, train_mean, color='blue', marker='o', markersize=5, label="training accuracy")
         plt.fill_between(train_sizes, train_mean + train_std, train_mean - train_std, alpha=0.15, color="blue")
         plt.plot(train_sizes, test_mean, color='green', marker='s', markersize=5, label="validation accuracy" )
@@ -81,18 +110,20 @@ class Tree():
         plt.tight_layout()
         plt.ylim([0.5, 1.0])
         plt.savefig(save_file_name)
+        plt.close()
 
-    def decision_tree_classifier(self, save_file_name=""):
+    def decision_tree_classifier(self, save_file_name="", max_depth=2):
         # 決定木による学習
-        print("============== 決定木 ===============")
+        print("============== 決定木(max_depth={}) ===============".format(max_depth))
 
         X_train,X_test,y_train,y_test = self.train_test_data_split(random_state=0, test_size=0.3)
         X_train_std, X_test_std = self.std_X(X_train, X_test)
 
-        treeModel = DecisionTreeClassifier(max_depth=3, random_state=0)
+        treeModel = DecisionTreeClassifier(max_depth=max_depth, random_state=0)
         treeModel.fit(X_train_std, y_train)
-        print("訓練データ： ", metrics.accuracy_score(y_train, treeModel.predict(X_train_std)))
-        print("テストデータ： ", metrics.accuracy_score(y_test, treeModel.predict(X_test_std)))
+        print("訓練データ(data_size:{})： ".format(len(X_train_std)), metrics.accuracy_score(y_train, treeModel.predict(X_train_std)))
+        print('訓練データ：Confusion matrix:\n{}'.format(confusion_matrix(y_train, treeModel.predict(X_train_std))))
+        print("テストデータ(data_size:{})： ".format(len(X_test_std)), metrics.accuracy_score(y_test, treeModel.predict(X_test_std)))
         print('テストデータ：Confusion matrix:\n{}'.format(confusion_matrix(y_test, treeModel.predict(X_test_std))))
 
         print("決定木による各項目における重要度")
@@ -100,9 +131,10 @@ class Tree():
         print(importance)
         treeModel.fit(self.X, self.y)
         predicted = pd.DataFrame({'TreePredicted':treeModel.predict(self.X)})
-        if True:
-            viz = dtreeviz(treeModel, self.X, self.y, target_name='score_dummy', feature_names=list(self.X.keys()), class_names=["high", "low"])
-            viz.save(save_file_name)
+        plt.figure()
+        viz = dtreeviz(treeModel, self.X, self.y, target_name='score_dummy', feature_names=list(self.X.keys()), class_names=["high", "low"])
+        viz.save(save_file_name)
+        plt.close()
         print("============== end ===============")
 
     def logistic_regression(self):
@@ -118,11 +150,6 @@ class Tree():
         print("testデータ:", metrics.accuracy_score(y_test, logisticModel.predict(X_test_std)))
 
         print('\n')
-        if False:
-            plt.scatter(data_predicted['word_length'],data_predicted['recommend_rank'], c=data_predicted['LogisPredicted'])
-            plt.xlabel('bm25')
-            plt.ylabel('recommend_rank')
-            plt.show()
         print("============== end ===============")
 
 def drop_column(df):
--- main.py
+++ main.py
@@ -6,16 +6,45 @@ df = preprocess(df)
 X = df.drop([
         "score_dummy", "report_created_date", "bm25_sum",
         "type_id", "shokushu_id", "score_std",
-        "recommend_level",
-        #"tfidf_top_average", "recommend_level",
+        "recommend_level", "tfidf_top_average",
+        "info_date", "write_date", "first_date", "second_date", "final_date", "decision_date",
+        #'identification_word_count', 'is_match_keywords',
+        #'most_highest_similarity', 'recommend_rank', 'report_created_datetime',
+        #'similarity_sum', 'tfidf_sum', 'word_length'
     ], axis=1) # score_dummy以外の列を抽出
-print(X.keys())
+#print(X.keys())
 y = df["score_dummy"].astype(int) # score_dummyの列を抽出
 tree = Tree(X, y)
 for i in range(2, 10):
     tree.decision_tree_classifier(save_file_name="decision_tree_image/decision_tree_questionnaire_{}.pdf".format(i), max_depth=i)
     tree.learning_curve_show(save_file_name="./decision_tree_image/qustion_learning_curve_{}.pdf".format(i), max_depth=i)
-tree.cross_validation()
+    tree.cross_validation(save_file_name="decision_tree_image/cross_decision_tree_{}.pdf".format(i), max_depth=i)
+
 tree.grid_search()
 tree.logistic_regression()
+
+
+
+
+print(X.keys())
+y = df["score_dummy"].astype(int) # score_dummyの列を抽出
+tree = Tree(X, y)
+import pandas as pd
+train_test = {}
+for index_y, max_depth in enumerate(range(2, 10)):
+    train_test[index_y] = []
+    for index_x, i in enumerate(range(0,10)):
+        train_test[index_y].append(tree.tmp_cross_validation(max_depth=max_depth, random_state=i))
+        #train_test[index_y].append(tree.tmp_random_forest(min_samples_leaf=max_depth,random_state=i))
+
+for t in train_test.values():
+    train = 0
+    test = 0
+    size = len(t)
+    for value in t:
+        train += value["train"]
+        test += value["test"]
+
+    print("=======")
+    print((train / size) - (test / size))
 
