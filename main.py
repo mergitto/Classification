8731aad42085b3714496455605e028dfe1cd5d45
mergitto <merugitosu@gmail.ccom>
2018-12-03 13:42:31 +0900

    全ての評価データは悪さをすることの方が多いように体感しているので一旦コメントアウトしておく

--- main.py
+++ main.py
@@ -98,55 +98,55 @@ tree.grid_search()
 tree.logistic_regression()
 
 
-df = pd.read_csv('./questionnaire_evaluations_preprocessed_all.csv')
-drop_list = [
-        "report_created_date", "bm25_sum",
-        "type_id", "shokushu_id", "score_std", "recommend_level", "tfidf_top_average",
-        "info_date", "write_date", "first_date", "second_date", "final_date", "decision_date",
-        "advice_divide_mecab", "bm25_average", "course_code", "created",
-        "keywords", "modified", "score", "score_min_max", "search_word",
-        "search_word_wakati", "topic", "st_no",
-        "is_good", "evaluation_id", "recommend_formula",
-        "most_highest_similarity",
-        #'count_selection', 'diff_date', 'first_final_diff_days',
-        #'identification_word_count', 'is_match_keywords',
-        #'most_highest_similarity', 'recommend_rank', 'report_created_datetime',
-        #'similarity_sum', 'tfidf_sum', 'word_length'
-    ] # 不必要なカラム
-tree = Tree(df)
-tree.add_dummy_score()
-tree.drop_columns(drop_list)
-tree.set_X_and_y(objective_key="score_dummy")
-
-for i in range(0, 8):
-    current_max_depth = max_depth[i]
-    current_C = C[i]
-    print(i+1, "回目の処理")
-    tree.learning_curve_show(
-            save_file_name="./tree_image_all/qustion_learning_curve_{}_{}.pdf".format("decision",current_max_depth),
-            max_depth=current_max_depth,
-            clf_name="decision")
-    tree.learning_curve_show(
-            save_file_name="./tree_image_all/qustion_learning_curve_{}_{}.pdf".format("regression",current_C),
-            C=current_C,
-            clf_name="regression")
-    tree.learning_curve_show(
-            save_file_name="./tree_image_all/qustion_learning_curve_{}_{}.pdf".format("random_forest",current_max_depth),
-            max_depth=current_max_depth,
-            clf_name="random_forest")
-    tree.decision_tree_classifier(save_file_name="tree_image_all/decision_tree_questionnaire_{}.pdf".format(current_max_depth), max_depth=current_max_depth)
-    tree.cross_validation(save_file_name="tree_image_all/cross_decision_tree_{}.pdf".format(current_max_depth), max_depth=current_max_depth)
-    tree.random_forest(random_state=i, max_depth=current_max_depth)
-
-# validation_curveの描画
-tree.validation_curve_show( "./tree_image_all/validation_curve_{}_C.pdf".format("regression"),
-        param_range=C, param_name="logisticregression__C", clf_name="regression")
-tree.validation_curve_show( "./tree_image_all/validation_curve_{}_max_depth.pdf".format("decision"),
-        param_range=max_depth, param_name="decisiontreeclassifier__max_depth", clf_name="decision")
-tree.validation_curve_show( "./tree_image_all/validation_curve_{}_max_depth.pdf".format("random_forest"),
-        param_range=max_depth, param_name="randomforestclassifier__max_depth", clf_name="random_forest")
-
-tree.grid_search()
-tree.logistic_regression()
+#df = pd.read_csv('./questionnaire_evaluations_preprocessed_all.csv')
+#drop_list = [
+#        "report_created_date", "bm25_sum",
+#        "type_id", "shokushu_id", "score_std", "recommend_level", "tfidf_top_average",
+#        "info_date", "write_date", "first_date", "second_date", "final_date", "decision_date",
+#        "advice_divide_mecab", "bm25_average", "course_code", "created",
+#        "keywords", "modified", "score", "score_min_max", "search_word",
+#        "search_word_wakati", "topic", "st_no",
+#        "is_good", "evaluation_id", "recommend_formula",
+#        "most_highest_similarity",
+#        #'count_selection', 'diff_date', 'first_final_diff_days',
+#        #'identification_word_count', 'is_match_keywords',
+#        #'most_highest_similarity', 'recommend_rank', 'report_created_datetime',
+#        #'similarity_sum', 'tfidf_sum', 'word_length'
+#    ] # 不必要なカラム
+#tree = Tree(df)
+#tree.add_dummy_score()
+#tree.drop_columns(drop_list)
+#tree.set_X_and_y(objective_key="score_dummy")
+#
+#for i in range(0, 8):
+#    current_max_depth = max_depth[i]
+#    current_C = C[i]
+#    print(i+1, "回目の処理")
+#    tree.learning_curve_show(
+#            save_file_name="./tree_image_all/qustion_learning_curve_{}_{}.pdf".format("decision",current_max_depth),
+#            max_depth=current_max_depth,
+#            clf_name="decision")
+#    tree.learning_curve_show(
+#            save_file_name="./tree_image_all/qustion_learning_curve_{}_{}.pdf".format("regression",current_C),
+#            C=current_C,
+#            clf_name="regression")
+#    tree.learning_curve_show(
+#            save_file_name="./tree_image_all/qustion_learning_curve_{}_{}.pdf".format("random_forest",current_max_depth),
+#            max_depth=current_max_depth,
+#            clf_name="random_forest")
+#    tree.decision_tree_classifier(save_file_name="tree_image_all/decision_tree_questionnaire_{}.pdf".format(current_max_depth), max_depth=current_max_depth)
+#    tree.cross_validation(save_file_name="tree_image_all/cross_decision_tree_{}.pdf".format(current_max_depth), max_depth=current_max_depth)
+#    tree.random_forest(random_state=i, max_depth=current_max_depth)
+#
+## validation_curveの描画
+#tree.validation_curve_show( "./tree_image_all/validation_curve_{}_C.pdf".format("regression"),
+#        param_range=C, param_name="logisticregression__C", clf_name="regression")
+#tree.validation_curve_show( "./tree_image_all/validation_curve_{}_max_depth.pdf".format("decision"),
+#        param_range=max_depth, param_name="decisiontreeclassifier__max_depth", clf_name="decision")
+#tree.validation_curve_show( "./tree_image_all/validation_curve_{}_max_depth.pdf".format("random_forest"),
+#        param_range=max_depth, param_name="randomforestclassifier__max_depth", clf_name="random_forest")
+#
+#tree.grid_search()
+#tree.logistic_regression()
 
 
--- main_svm.py
+++ main_svm.py
@@ -49,8 +49,10 @@ classification.validation_curve_show(save_file_name="./classifier_image/validati
         param_range=n_neighbors, param_name="kneighborsclassifier__n_neighbors", clf_name="knn")
 
 
-df = pd.read_csv('./questionnaire_evaluations_preprocessed_all.csv')
+df = pd.read_csv('./questionnaire_all_evaluations_preprocessed_from_20181030.csv')
 classification = Classification(df)
+drop_na_list = ["score"]
+classification.drop_na(drop_na_list=drop_na_list)
 drop_list = [
         "report_created_date", "bm25_sum",
         "type_id", "shokushu_id", "score_std",
@@ -68,7 +70,9 @@ drop_list = [
     ] # 不必要なカラム
 classification.add_dummy_score()
 classification.drop_columns(drop_list)
+print('hoge')
 classification.set_X_and_y(objective_key="score_dummy")
+print('hoge')
 classification.simple_svm()
 classification.std_svm()
 classification.grid_svm()
@@ -76,23 +80,73 @@ classification.grid_svm()
 for i in range(0, 8):
     print("C: ", C[i], "gamma: ", gamma[i])
     classification.learning_curve_show(
-            save_file_name="./classifier_image_all/learning_curve_{}_C{}_G{}.pdf".format("svm", C[i], gamma[i]),
+            save_file_name="./classifier_image_from_20181030/learning_curve_{}_C{}_G{}.pdf".format("svm", C[i], gamma[i]),
             clf_name="svm", C=C[i], gamma=gamma[i])
     classification.learning_curve_show(
-            save_file_name="./classifier_image_all/learning_curve_{}_n_nei{}.pdf".format("knn", n_neighbors[i]),
+            save_file_name="./classifier_image_from_20181030/learning_curve_{}_n_nei{}.pdf".format("knn", n_neighbors[i]),
             clf_name="knn", n_neighbors=n_neighbors[i])
-classification.learning_curve_show( save_file_name="./classifier_image_all/learning_curve_{}_C_small_G_small.pdf".format("svm"),
+classification.learning_curve_show( save_file_name="./classifier_image_from_20181030/learning_curve_{}_C_small_G_small.pdf".format("svm"),
         clf_name="svm", C=0.001, gamma=0.001)
-classification.learning_curve_show( save_file_name="./classifier_image_all/learning_curve_{}_C_small_G_large.pdf".format("svm"),
+classification.learning_curve_show( save_file_name="./classifier_image_from_20181030/learning_curve_{}_C_small_G_large.pdf".format("svm"),
         clf_name="svm", C=0.001, gamma=1000)
-classification.learning_curve_show( save_file_name="./classifier_image_all/learning_curve_{}_C_large_G_small.pdf".format("svm"),
+classification.learning_curve_show( save_file_name="./classifier_image_from_20181030/learning_curve_{}_C_large_G_small.pdf".format("svm"),
         clf_name="svm", C=1000, gamma=0.001)
-classification.learning_curve_show( save_file_name="./classifier_image_all/learning_curve_{}_C_large_G_large.pdf".format("svm"),
+classification.learning_curve_show( save_file_name="./classifier_image_from_20181030/learning_curve_{}_C_large_G_large.pdf".format("svm"),
         clf_name="svm", C=1000, gamma=1000)
 
-classification.validation_curve_show(save_file_name="./classifier_image_all/validation_curve_svc_C.pdf",
+classification.validation_curve_show(save_file_name="./classifier_image_from_20181030/validation_curve_svc_C.pdf",
         param_range=C, param_name="svc__C", clf_name="svm")
-classification.validation_curve_show(save_file_name="./classifier_image_all/validation_curve_svc_gamma.pdf",
+classification.validation_curve_show(save_file_name="./classifier_image_from_20181030/validation_curve_svc_gamma.pdf",
         param_range=gamma, param_name="svc__gamma", clf_name="svm")
-classification.validation_curve_show(save_file_name="./classifier_image_all/validation_curve_knn_n_nei.pdf",
+classification.validation_curve_show(save_file_name="./classifier_image_from_20181030/validation_curve_knn_n_nei.pdf",
         param_range=n_neighbors, param_name="kneighborsclassifier__n_neighbors", clf_name="knn")
+
+
+#df = pd.read_csv('./questionnaire_evaluations_preprocessed_all.csv')
+#classification = Classification(df)
+#drop_list = [
+#        "report_created_date", "bm25_sum",
+#        "type_id", "shokushu_id", "score_std",
+#        "recommend_level", "tfidf_top_average",
+#        "info_date", "write_date", "first_date", "second_date", "final_date", "decision_date",
+#        "advice_divide_mecab", "bm25_average", "course_code", "created",
+#        "keywords", "modified", "score", "score_min_max", "search_word",
+#        "search_word_wakati", "topic", "st_no",
+#        "is_good", "evaluation_id", "recommend_formula",
+#        "most_highest_similarity",
+#        #'count_selection', 'diff_date', 'first_final_diff_days',
+#        #'identification_word_count', 'is_match_keywords',
+#        #'most_highest_similarity', 'recommend_rank', 'report_created_datetime',
+#        #'similarity_sum', 'tfidf_sum', 'word_length'
+#    ] # 不必要なカラム
+#classification.add_dummy_score()
+#classification.drop_columns(drop_list)
+#classification.set_X_and_y(objective_key="score_dummy")
+#classification.simple_svm()
+#classification.std_svm()
+#classification.grid_svm()
+#
+#for i in range(0, 8):
+#    print("C: ", C[i], "gamma: ", gamma[i])
+#    classification.learning_curve_show(
+#            save_file_name="./classifier_image_all/learning_curve_{}_C{}_G{}.pdf".format("svm", C[i], gamma[i]),
+#            clf_name="svm", C=C[i], gamma=gamma[i])
+#    classification.learning_curve_show(
+#            save_file_name="./classifier_image_all/learning_curve_{}_n_nei{}.pdf".format("knn", n_neighbors[i]),
+#            clf_name="knn", n_neighbors=n_neighbors[i])
+#classification.learning_curve_show( save_file_name="./classifier_image_all/learning_curve_{}_C_small_G_small.pdf".format("svm"),
+#        clf_name="svm", C=0.001, gamma=0.001)
+#classification.learning_curve_show( save_file_name="./classifier_image_all/learning_curve_{}_C_small_G_large.pdf".format("svm"),
+#        clf_name="svm", C=0.001, gamma=1000)
+#classification.learning_curve_show( save_file_name="./classifier_image_all/learning_curve_{}_C_large_G_small.pdf".format("svm"),
+#        clf_name="svm", C=1000, gamma=0.001)
+#classification.learning_curve_show( save_file_name="./classifier_image_all/learning_curve_{}_C_large_G_large.pdf".format("svm"),
+#        clf_name="svm", C=1000, gamma=1000)
+#
+#classification.validation_curve_show(save_file_name="./classifier_image_all/validation_curve_svc_C.pdf",
+#        param_range=C, param_name="svc__C", clf_name="svm")
+#classification.validation_curve_show(save_file_name="./classifier_image_all/validation_curve_svc_gamma.pdf",
+#        param_range=gamma, param_name="svc__gamma", clf_name="svm")
+#classification.validation_curve_show(save_file_name="./classifier_image_all/validation_curve_knn_n_nei.pdf",
+#        param_range=n_neighbors, param_name="kneighborsclassifier__n_neighbors", clf_name="knn")
+
